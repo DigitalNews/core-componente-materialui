03b96ed3995e3058496860d25d08a08ec6148e99
rony <mylovepre16@gmail.com>
2020-09-22 15:27:42 -0500

    styleguide configuration

---
+++ styleguide.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  assetsDir: "public/",
+  skipComponentsWithoutExample: true,
+  ignore: ["**/*index.js"],
+  pagePerSection: true,
+  usageMode: "expand",
+  template: {
+    head: {
+      scripts: [
+        {
+          src: "https://kit.fontawesome.com/4c273e6d43.js",
+        },
+      ],
+      links: [
+        {
+          rel: "stylesheet",
+          href:
+            "https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&display=swap",
+        },
+      ],
+    },
+  },
+  sections: [
+    {
+      name: "Introduction",
+      content: "./README.md",
+      sectionDepth: 1,
+      exampleMode: "hide",
+      usageMode: "hide",
+    },
+    {
+      name: "Atoms",
+      sectionDepth: 2,
+      exampleMode: "collapse",
+      usageMode: "expand",
+      components: "./src/components/atoms/**/*.ts",
+    },
+    {
+      name: "Molecules",
+      sectionDepth: 2,
+      exampleMode: "collapse",
+      usageMode: "expand",
+      components: "./src/components/molecules/**/*.js",
+    },
+    {
+      name: "Organisms",
+      sectionDepth: 2,
+      exampleMode: "collapse",
+      usageMode: "expand",
+      components: "./src/components/organisms/**/*.js",
+    },
+  ],
+  webpackConfig: Object.assign({}, require("./webpack.config"), {}),
+  propsParser: require("react-docgen-typescript").withDefaultConfig({
+    savePropValueAsString: true,
+  }).parse
+};
