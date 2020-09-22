a0543ce104770867c3c92091915fa4e7d5adde77
rony <mylovepre16@gmail.com>
2020-09-22 17:16:06 -0500

    export OverlapedImage

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,3 +1,4 @@
 export { default as CountUpNumber } from "./CountUpNumber";
 export { default as DescriptionCta } from "./DescriptionCta";
 export { default as IconAlternate } from "./IconAlternate";
+export { default as OverlapedImages } from "./OverlapedImages";
