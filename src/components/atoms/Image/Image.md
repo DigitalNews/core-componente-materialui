50f898ee844a67f4503da9dced95c4ac77e91362
rony <mylovepre16@gmail.com>
2020-09-22 15:26:35 -0500

    add  image components atoms

---
+++ src/components/atoms/Image/Image.md
@@ -0,0 +1,10 @@
+```jsx
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import imageprueba from "../../../images/ilustrations/brand-platform.svg";
+import theme from "../../../theme";
+
+<ThemeProvider theme={theme}>
+  <Image src={imageprueba} />
+</ThemeProvider>;
+```
---
+++ src/components/atoms/Image/Image.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  LazyLoadImage,
+  LazyLoadImageProps,
+} from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/opacity.css";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+    height: "100%",
+  },
+  dBlock: {
+    display: "block",
+  },
+}));
+
+interface IImageProps {
+  /**
+   * External classes
+   */
+  className?: string;
+  /**
+   * Source of the image
+   */
+  src: string;
+  /**
+   * Source set for the responsive images
+   */
+  srcSet?: string;
+  /**
+   * Image title
+   */
+  alt?: string;
+  /**
+   * Should lazy load the image
+   */
+  lazy?: boolean;
+  /**
+   * Additional properties to pass to the LazyLoadImage component
+   */
+  lazyProps?: LazyLoadImageProps;
+}
+
+/**
+ * Component to display the images
+ *
+ * @param {Object} props
+ */
+const Image: React.FunctionComponent<IImageProps> = (props) => {
+  const {
+    src,
+    srcSet,
+    alt = "...",
+    lazy = true,
+    lazyProps = { width: "auto", height: "auto" },
+    className,
+    ...rest
+  } = props;
+
+  const classes = useStyles();
+  if (lazy) {
+    return (
+      <LazyLoadImage
+        className={clsx("image", classes.root, classes.dBlock, className)}
+        alt={alt}
+        src={src}
+        srcSet={srcSet}
+        effect="opacity"
+        {...lazyProps}
+        {...rest}
+      />
+    );
+  }
+
+  return (
+    <img
+      className={clsx("image", classes.root, className)}
+      alt={alt}
+      src={src}
+      srcSet={srcSet}
+      {...rest}
+    />
+  );
+};
+
+export default Image;
