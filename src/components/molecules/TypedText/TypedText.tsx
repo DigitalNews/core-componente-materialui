abcf933dec6cef17ddaa3a238cc10d4801165516
rony <mylovepre16@gmail.com>
2020-09-22 20:20:56 -0500

    add typedText components molecules

---
+++ src/components/molecules/TypedText/TypedText.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import clsx from "clsx";
+import Typed, { ReactTypedProps } from "react-typed";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, TypographyProps } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+interface ITypedTextProps extends TypographyProps {
+  /**
+   * External classes
+   */
+  className?: string;
+  /**
+   * react-typed properties. For more info visit https://www.npmjs.com/package/react-typed
+   */
+  typedProps: ReactTypedProps;
+  /**
+   * component use for Typography
+   */
+  component: any;
+}
+/**
+ * Component to display the typed animated texts
+ *
+ * @param {Object} props
+ */
+
+const TypedText: React.FunctionComponent<ITypedTextProps> = (props) => {
+  const { className, typedProps, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <Typography
+      className={clsx("typed-text", classes.root, className)}
+      {...rest}
+    >
+      <Typed className="typed-text__item" {...typedProps} />
+    </Typography>
+  );
+};
+
+export default TypedText;
