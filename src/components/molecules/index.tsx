49df3a0085273fb744b46d52428740343b187667
rony <mylovepre16@gmail.com>
2020-09-22 20:22:57 -0500

    export typedtext

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -5,3 +5,4 @@ export { default as OverlapedImages } from "./OverlapedImages";
 export { default as SectionHeader } from "./SectionHeader";
 export { default as SwiperImage } from "./SwiperImage";
 export { default as SwiperNumber } from "./SwiperNumber";
+export { default as TypedText } from "./TypedText";
