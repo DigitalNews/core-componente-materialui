8d5ae964f7d49176e84a7eadd1c98b4f7275c19d
rony <mylovepre16@gmail.com>
2020-09-22 20:01:34 -0500

    export swipernumber

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -3,4 +3,5 @@ export { default as DescriptionCta } from "./DescriptionCta";
 export { default as IconAlternate } from "./IconAlternate";
 export { default as OverlapedImages } from "./OverlapedImages";
 export { default as SectionHeader } from "./SectionHeader";
-export { default as SwiperImage } from './SwiperImage';
+export { default as SwiperImage } from "./SwiperImage";
+export { default as SwiperNumber } from "./SwiperNumber";
